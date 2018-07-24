d34187caf2615422ccf5b851c4b795e0c6417d9c
Nikan <32056409+Nikan888@users.noreply.github.com>
2018-07-24 17:25:13 +0300

    Task_Exceptions

---
+++ Task_Exception/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Worker> list = new List<Worker>
+            {
+                new Worker("Yury", "Bolbukou", 18, "Oktyabrya, 48a", "BI", "trainee", "+375293595413", 25),
+                new Worker("Stanislav", "Ivanov", 25, "Gagarina, 20", "BI", "junior", "+375441158736", 154),
+                new Worker("QWERTYUI", "YCFVGYBKHN", 22, "RDCTFVGBHN", "CTVGYBHUN", "OIJL", "684526454", 98),
+                new Worker("fdghjkjh", "dfghjjkh", 20, "efdghkj", "drtyuk", "iokjhg", "123456789", 74),
+                new Worker("Ivan", "Apekunov", 18, "Chekalova, 22", "BI", "trainee", "+375291748933", 120)
+            };
+            try
+            {
+                list.Add(new Worker("Stanislav", "Verevkin", 12, "Masurova, 25", "BI", "trainee", "+375298745214", 50));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("List of workers:");
+            foreach (var e in list)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_Exception/Worker.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Exception
+{
+    public class Worker
+    {
+        string firstName;
+        string lastName;
+        int age;
+        string homeAdress;
+        string department;
+        string post;
+        string workPhoneNumber;
+        decimal salary;
+
+        public string FirstName { get => firstName; private set => firstName = value; }
+        public string LastName { get => lastName; private set => lastName = value; }
+        public string HomeAdress { get => homeAdress; private set => homeAdress = value; }
+        public string Department { get => department; private set => department = value; }
+        public string Post { get => post; private set => post = value; }
+        public string WorkPhoneNumber { get => workPhoneNumber; private set => workPhoneNumber = value; }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                if (value <= 14)
+                {
+                    throw new Exception("Age must be more than 13");
+                }
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Salary must be more than 0");
+                }
+                salary = value;
+            }
+        }
+
+        public Worker(string firstName, string lastName, int age, string homeAdress,
+            string department, string post, string workPhoneNumber, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            HomeAdress = homeAdress;
+            Department = department;
+            Post = post;
+            WorkPhoneNumber = workPhoneNumber;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return (FirstName + ';' + LastName + ';' + Age + ';' + HomeAdress + ';' + Department
+                + ';' + Post + ';' + WorkPhoneNumber + ';' + Salary);
+        }
+    }
+}
